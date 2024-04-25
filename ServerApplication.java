4fcb1af86892dc70177d69e74432e1bc163d3844
bport92 <111293744+bport92@users.noreply.github.com>
2024-04-25 13:50:59 -0400

    Add files via upload

---
+++ ServerApplication.java
@@ -0,0 +1,50 @@
+package com.snhu.sslserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
+
+@RestController
+class ServerController {
+    @RequestMapping("/hash")
+    public String myHash() {
+        String data = "Brandon Porter";
+        String uniqueData = "Hello " + data + "!";
+
+        String checksum = generateChecksum(uniqueData);
+
+        return "<p>Data: " + uniqueData + "</p>"
+             + "<p>Checksum: " + checksum + "</p>";
+    }
+
+    private String generateChecksum(String data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+}
